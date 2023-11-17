a546de18b4c1cb4260316624d980048db53feb2b
RAHUL N P <127581341+Rahulraveendrannp@users.noreply.github.com>
2023-11-17 10:02:02 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+
+import './globals.css'
+import { Footer, NavBar } from '@/components'
+
+
+
+
+export const metadata: Metadata = {
+  title: 'Car Hut',
+  description: 'Discover  the best cars',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  )
+}
---
+++ components/index.tsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+import CarDetails from "./CarDetails";
+
+export {CarDetails,Hero,CustomButton,NavBar,Footer,CustomFilter,SearchBar,SearchManufacturer,CarCard,ShowMore}
